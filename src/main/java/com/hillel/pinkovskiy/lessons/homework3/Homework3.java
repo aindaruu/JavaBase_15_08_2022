7b071487d327a73b1517172ed2b6cf58fd0cc78a
Pinkovskiy Andrii <reyjunior2006@gmail.com>
2022-10-09 22:43:51 +0300

    homework3

---
+++ src/main/java/com/hillel/pinkovskiy/lessons/homework3/Homework3.java
@@ -0,0 +1,15 @@
+package com.hillel.pinkovskiy.lessons.homework3;
+
+public class Homework3 {
+    public static void main(String[] args) {
+        int a = 15;
+        int b = 12;
+        int c = 6;
+
+        int sum = a + b;
+
+        int result = (a + b) * c;
+        System.out.println(result);
+
+    }
+}
